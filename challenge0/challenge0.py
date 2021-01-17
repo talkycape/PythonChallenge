6e44b882a89889fd87dfb1c09c5436fbe9c2e3c2
YACHIEH LAI <yachieh@sabrina-macbook-air.lan>
2021-01-17 10:37:26 -0800

    committed solution for first challenge

---
+++ challenge0/challenge0.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+print("hello world")
+solution = 2**38
+target_url = 'http://www.pythonchallenge.com/pc/def/%s.html' % solution
+print("The value of 2**38 is: " + str(solution))
+print("The target URL is therefore: %s" % target_url)
+
+# If new is 0, the url is opened in the same browser window if possible.
+# If new is 1, a new browser window is opened if possible.
+# If new is 2, a new browser page ("tab") is opened if possible.
+
+webbrowser.open(target_url, new=2)
