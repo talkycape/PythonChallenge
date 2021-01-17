1f37e577809c4a8414a34acdd1302b6ec44b90a4
Y. Lai <yachieh@gmail.com>
2021-01-17 10:41:54 -0800

    added more comments

--- challenge0/challenge0.py
+++ challenge0/challenge0.py
@@ -10,4 +10,5 @@ print("The target URL is therefore: %s" % target_url)
 # If new is 1, a new browser window is opened if possible.
 # If new is 2, a new browser page ("tab") is opened if possible.
 
+# finish by opening up webpage of next step of challenge
 webbrowser.open(target_url, new=2)
